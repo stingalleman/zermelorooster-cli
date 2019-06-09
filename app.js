66182f71b59e2fe3c6a200023e82ef279f51f145
Sting Alleman <stingalleman@icloud.com>
2019-06-09 02:35:31 +0200

    login cmd + almost working week schedule cmd

--- app.js
+++ app.js
@@ -1,41 +1,74 @@
 #! /usr/bin/env node
 
-const program = require('commander')
-const request = require('request')
-const fs = require('fs')
-const config = require('./lib/config.json')
+var program = require('commander');
+var request = require('request');
+var inquirer = require('inquirer');
+var chalk = require('chalk');
+var moment = require('moment')
+var fs = require('fs');
+var config = require('./lib/config.json');
+
 
 program
     .version('0.1.0')
+    .description('CLI wrapper for Zermelo, developed by Sting Alleman')
 
 program
     .command('login')
-    .description('login to your zermelo account')
-    .action(function (login) {
-        const options = {
-            method: "POST",
-            url: "https://alfrink.zportal.nl",
-            headers: {
-                grant_type: "authorization_code",
-                code: config.authCode
+    .description('login to zermelo')
+    .action(function () {
+        var questions = [{
+                type: "input",
+                name: "token",
+                message: "What's your Zermelo token?"
+            },
+            {
+                type: "input",
+                name: "schoolName",
+                message: "What's your zportal school name?"
             }
+        ]
+        inquirer.prompt(questions).then(tokenAnswer => {
+            var newConfig = {
+                ...config,
+                ...tokenAnswer
+            };
+            var newConfigJSON = JSON.stringify(newConfig, null, '  ');
+            fs.writeFileSync('./lib/config.json', newConfigJSON)
+        })
+    });
+
+program
+    .command('week')
+    .description('show your appointments for the week')
+    .action(function () {
+        var ew = moment().endOf('week').unix()
+        var sw = moment().startOf('week').unix()
+        console.log(ew + ' ' + sw)
+        //https://alfrink.zportal.nl/api/v3/appointments?user=~me&start=1560246900&end=1560249900&access_token=62du45uvg5uqvqct5t0a9tmpgd
+        var options = {
+            url: 'https://' + config.schoolName + '.zportal.nl/api/v3/appointments?user=~me&start=' + sw + '&end=' + ew + '&access_token=' + config.token
         }
 
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
-                var info = JSON.parse(body);
-                console.log(info)
-            } else {
-                console.log("nothing!")
+                var data = JSON.stringify(body, null, '  ')
+                fs.writeFileSync('./lib/data.json', data)
+                console.log(body)
             }
         }
-        request(options, callback)
+        console.log(options.url)
+        request(options, callback);
     });
- /*program
-    .command(schedule)
-    .description('view your own schedule for this week')
-    .action(function (schedule) {
 
-    }) */
+
+
+
+/*program
+   .command(schedule)
+   .description('view your own schedule for this week')
+   .action(function (schedule) {
+
+   }) */
 
 program.parse(process.argv);
